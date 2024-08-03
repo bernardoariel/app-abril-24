cc003d83661fd0ee6c3bc5d67312cc176950b327
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-08-03 09:32:22 -0300

    primer commit

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <!-- RouterView -->
+  <router-view />
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import TopMenu from '../components/TopMenu.vue';
+import CustomFooter from '../components/CustomFooter.vue';
+</script>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import ShopLayout from '../modules/shop/layouts/ShopLayout.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'shop',
+      component: ShopLayout,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: () => import('../modules/shop/views/HomeView.vue'),
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
