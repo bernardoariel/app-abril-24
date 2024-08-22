e1dffbb2ce213950ef71604ee85bfddaa0a350ed
GabrielNavas <cgnavas98@gmail.com>
2024-08-22 07:39:57 -0300

    debounce

---
+++ src/views/SearchProductView copy.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div class="w-full max-w-xs">
+      <input
+        type="text"
+        placeholder="Ingrese un producto a buscar"
+        class="input input-bordered w-full mb-4"
+        v-model="productToSearch"
+      />
+      <button @click="() => fetchProducts(productToSearch)" class="btn btn-primary w-full">Buscar</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useProductStore } from '@/store/useProductStore';
+import { ref } from 'vue';
+const productToSearch = ref('')
+const productStore = useProductStore();
+const { fetchProducts } = productStore;
+
+</script>
