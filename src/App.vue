aaadbc4f23d996bc4a0b7e4579f37d4fe246a5a0
GabrielNavas <cgnavas98@gmail.com>
2024-08-24 20:10:33 -0300

    quitando scroll en searchView

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,20 @@ import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
-  <div class="bg-gray-100 min-h-screen">
+  <div class="bg-gray-100 min-h-screen flex flex-col">
     <NavHeader />
-    <div class="max-w-custom mx-auto px-4 bg-gray-100">
-      <RouterView />
+    <div class="flex-grow">
+      <div class="h-full">
+        <RouterView />
+      </div>
     </div>
     <FooterComponent />
   </div>
 </template>
+
+<style scoped>
+.flex-grow {
+  width: 100%;
+  height: calc(100vh - 128px);
+}
+</style>
