c3ea2c0cc40357ef2fcb9c0aba6ea85389038574
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-08-30 10:45:39 -0300

    Fix Icons

--- src/components/icons/SearchIcon.vue
+++ src/components/icons/SearchIcon.vue
@@ -1,25 +1,21 @@
 <template>
-  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40" heigth="40">
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
     <g id="SVGRepo_iconCarrier">
       <path
-        d="M14 4C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12V13M10 4C6.22876 4 4.34315 4 3.17157 5.17157C2 6.34315 2 8.22876 2 12C2 15.7712 2 17.6569 3.17157 18.8284C4.34315 20 6.22876 20 10 20H13"
-        stroke="currentColor"
+        d="M22 14V12C22 8.22876 22 6.34315 20.8284 5.17157C19.6569 4 17.7712 4 14 4M14 22H10C6.22876 22 4.34315 22 3.17157 20.8284C2 19.6569 2 17.7712 2 14V12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4"
+        stroke="#1C274C"
         stroke-width="1.5"
         stroke-linecap="round"
       ></path>
-      <path d="M10 16H6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"></path>
-      <circle cx="18" cy="17" r="3" stroke="currentColor" stroke-width="1.5"></circle>
+      <path d="M7 4V2.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path>
+      <path d="M17 4V2.5" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path>
+      <circle cx="18" cy="18" r="3" stroke="#1C274C" stroke-width="1.5"></circle>
+      <path d="M20.5 20.5L22 22" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path>
       <path
-        d="M20.5 19.5L21.5 20.5"
-        stroke="currentColor"
-        stroke-width="1.5"
-        stroke-linecap="round"
-      ></path>
-      <path
-        d="M2 10L7 10M22 10L11 10"
-        stroke="currentColor"
+        d="M21.5 9H16.625H10.75M2 9H5.875"
+        stroke="#1C274C"
         stroke-width="1.5"
         stroke-linecap="round"
       ></path>
