97d015627dbb7429cc416d6c218b1bf99f69a5dd
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-09-07 17:53:48 -0300

    Agregando dockerfile prod y dev

--- src/modules/sqlserver/products/components/ProductList.vue
+++ src/modules/sqlserver/products/components/ProductList.vue
@@ -8,28 +8,67 @@
     ]"
   >
     <div class="flex items-center justify-between">
-      <div class="flex flex-col ml-3">
-        <h3 class="font-medium leading-none">{{ prod.Producto }}</h3>
-        <p class="text-sm leading-none mt-1">{{ prod.Descripcion }}</p>
-        <p class="text-sm leading-none mt-1">{{ prod.Medida }}</p>
-        <p class="text-sm leading-none mt-1">{{ prod.Stock }} Unidades</p>
-        <p class="text-sm leading-none mt-1 font-semibold">{{ formatPrice(prod.Precio) }}</p>
+      <div class="flex flex-col w-full">
+        <div class="flex justify-between w-full">
+          <h3 class="font-medium text-lg">{{ prod.Producto }}</h3>
+          <h3 class="font-medium text-lg badge badge-ghost badge-outline mt-1">
+            {{ prod.CodProducto }}
+          </h3>
+        </div>
+        <p class="text-sm leading-none mt-1 text-center font-semibold">
+          {{ prod.Descripcion }}
+        </p>
+        <div class="flex items-center justify-between">
+          <!-- Imagen del producto -->
+          <div class="flex-shrink-0 w-16 h-16 bg-gray-200 rounded-md overflow-hidden">
+            <img
+              :src="prod.Imagen || imgDefault"
+              alt="Imagen del producto"
+              class="w-full h-auto object-cover rounded-md"
+            />
+          </div>
+
+          <!-- Información del producto (al centro) -->
+          <div class="flex flex-col text-center mx-4 space-y-2">
+            <!-- Fila: Medida y Marca -->
+            <div class="flex items-center justify-around space-x-4">
+              <p class="text-lg leading-none">{{ prod.Medida }}</p>
+              <p class="text-sm leading-none font-medium badge badge-neutral">
+                {{ findMarcasById(prod.CodMarca)?.Marca }}
+              </p>
+            </div>
+
+            <!-- Fila: Stock y Precio -->
+            <div class="flex items-center justify-around space-x-5">
+              <p class="text-2xl leading-none">{{ prod.Stock }} Unidades</p>
+              <p class="text-2xl leading-none font-semibold">
+                {{ formatPrice(prod.Precio) }}
+              </p>
+            </div>
+          </div>
+
+          <!-- Botón de acción (Ver Producto) -->
+          <div class="flex-shrink-0">
+            <router-link :to="`/product/${prod.CodProducto}`" class="absolute bottom-2 right-2">
+              <button
+                class="btn btn-accent w-16 h-auto px-0 py-2 text-white transition-colors duration-200 hover:bg-accent-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent"
+              >
+                Ver
+              </button>
+            </router-link>
+          </div>
+        </div>
       </div>
     </div>
-
-    <router-link :to="`/product/${prod.CodProducto}`" class="absolute bottom-2 right-2">
-      <button
-        class="btn btn-accent px-3 py-1 text-white transition-colors duration-200 hover:bg-accent-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent"
-      >
-        Ver {{ prod.CodProducto }}
-      </button>
-    </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { ProductsResponse } from '../interfaces/products.response';
 import { formatPrice } from '../../../../common/helpers/formatPrice';
+import { useMarcas } from '../../marcas/composable/useMarcas';
 
 const props = defineProps<{ productos: ProductsResponse[] }>();
+const { findMarcasById } = useMarcas();
+const imgDefault = import.meta.env.VITE_BASE_URL + '/src/assets/img/No_Image_Available.jpg';
 </script>
