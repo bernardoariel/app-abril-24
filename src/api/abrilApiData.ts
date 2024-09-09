c646dc56b561a694a722365c537719406273f245
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-09-09 10:32:44 -0300

    addesd

--- src/api/abrilApiData.ts
+++ src/api/abrilApiData.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const abrilApiData = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'http://149.50.144.81:3000',
+  baseURL: 'https://149.50.144.81:3000',
 });
 
 // baseURL: 'http://localhost:3000',
