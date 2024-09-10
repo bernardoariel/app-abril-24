ecda957911d17f708e70e6d1d6fc2b77885ac1b0
Ariel Note <arielbernardo@hotmail.com>
2024-09-09 22:54:50 -0300

    localhost

--- src/api/abrilApiData.ts
+++ src/api/abrilApiData.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const abrilApiData = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'https://149.50.144.81:3000',
+  baseURL: 'http://localhost:3000',
+  // baseURL: 'https://149.50.144.81:3000',
 });
 
 // baseURL: 'http://localhost:3000',
