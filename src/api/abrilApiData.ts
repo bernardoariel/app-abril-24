2bb4353ad56b6681249bc9cf032ee72802e48ea6
Ariel Bernardo <64559228+bernardoariel@users.noreply.github.com>
2024-10-02 14:07:26 -0300

    modified function abrilapidata

--- src/api/abrilApiData.ts
+++ src/api/abrilApiData.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const getApiBaseUrl = () => {
-  if (import.meta.env.MODE === 'production') return import.meta.env.VITE_API_BASE_URL_PROD;
-  if (window.location.href.includes('capacitor://'))
-    return import.meta.env.VITE_API_BASE_URL_CAPACITOR;
-
+  if (import.meta.env.MODE === 'production') {
+    if (window.location.href.includes('capacitor://')) {
+      return import.meta.env.VITE_API_BASE_URL_CAPACITOR;
+    }
+    return import.meta.env.VITE_API_BASE_URL_PROD;
+  }
   return import.meta.env.VITE_API_BASE_URL_LOCAL;
 };
-console.log(import.meta.env.MODE);
 
 const abrilApiData = axios.create({
   baseURL: getApiBaseUrl(),
