4ccb276f2d4853b18792da8b4afe3c58f24adce6
Ariel Hernán Bernardo <arielbernardo@hotmail.com.ar>
2025-03-29 13:27:49 -0300

    agregando compartir

--- src/modules/sqlserver/products/components/ProductCuotas.vue
+++ src/modules/sqlserver/products/components/ProductCuotas.vue
@@ -2,10 +2,15 @@
   <div class="card bg-white text-orangeCustom2 shadow-xl p-4 mb-16">
     <div
       role="alert"
-      class="alert mb-5 alert-warning flex justify-center items-center bg-orangeCustom3Translucent"
+      class="alert mb-5 alert-warning flex justify-between items-center bg-orangeCustom3Translucent"
     >
       <span class="font-bold text-center text-orangeCustom2"> {{ props.Producto }} </span>
-      <button class="btn btn-sm bg-blue-500 text-white ml-2" @click="exportToPDF">📄 PDF</button>
+      <button
+        class="btn bg-orangeCustom hover:bg-orangeCustom2 text-white ml-2"
+        @click="exportToPDF"
+      >
+        📄 PDF
+      </button>
     </div>
     <div role="alert" class="alert mb-5 bg-orange-300 flex justify-between">
       <span class="font-bold"> Contado = {{ formatPrice(precioContado) }} </span>
@@ -17,7 +22,8 @@
       <div class="collapse collapse-arrow bg-orange-200">
         <input type="checkbox" />
         <div class="collapse-title text-lg font-bold mb-4">
-          {{ findFormaPagoById(codTarjeta)?.FormaPago || 'Sin nombre' }} - {{ codTarjeta }}
+          {{ findFormaPagoById(codTarjeta)?.FormaPago || "Sin nombre" }} -
+          {{ codTarjeta }}
         </div>
         <div class="collapse-content">
           <table class="table w-full text-center">
@@ -40,7 +46,7 @@
                   {{
                     formatPrice(
                       (precioLista * (1 + (tarjeta.Interes / 100) * tarjeta.NCuota)) /
-                        tarjeta.NCuota,
+                        tarjeta.NCuota
                     )
                   }}
                 </td>
@@ -57,15 +63,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 // @ts-ignore
-import html2pdf from 'html2pdf.js';
-import { formatPrice } from '../../../../common/helpers/formatPrice';
-import { useFormaPagoPlanes } from '../../forma-pago-planes/composable/useFormaPagosPlanes';
-import { useFormaPago } from '../../forma-pago/composable/useFormaPago';
-import { useMarcas } from '../../marcas/composable/useMarcas';
-import jsPDF from 'jspdf';
-import autoTable from 'jspdf-autotable';
+import html2pdf from "html2pdf.js";
+import { formatPrice } from "../../../../common/helpers/formatPrice";
+import { useFormaPagoPlanes } from "../../forma-pago-planes/composable/useFormaPagosPlanes";
+import { useFormaPago } from "../../forma-pago/composable/useFormaPago";
+import { useMarcas } from "../../marcas/composable/useMarcas";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 // Definir las props
 interface Props {
@@ -85,10 +91,10 @@ interface AttrLoader {
 }
 const ConfigLoader: AttrLoader = {
   size: 80,
-  color: '#000',
+  color: "#000",
 };
 const props = withDefaults(defineProps<Props>(), {
-  Imagen: '',
+  Imagen: "",
 });
 
 // Precio de lista basado en la prop Precio
@@ -103,71 +109,95 @@ const precioDebito = computed(() => {
 const { formaPagoPlanes } = useFormaPagoPlanes();
 const { findFormaPagoById } = useFormaPago();
 const { findMarcasById } = useMarcas();
-const arrayCreditos = ['CRE', 'TNA', 'TNP', 'TVI']; // Códigos de crédito
+const arrayCreditos = ["CRE", "TNA", "TNP", "TVI"]; // Códigos de crédito
 const mostrarContenido = ref(true);
 
 const exportToPDF = () => {
   const doc = new jsPDF();
   let y = 20;
 
-  doc.setFont('times', 'normal'); // ✅ Fuente más compatible
-  doc.setFontSize(18);
-  doc.text(`Presupuesto - Fecha: ${new Date().toLocaleDateString()}`.normalize('NFC'), 105, y, {
-    align: 'center',
-  });
-
-  y += 10;
-  doc.setFontSize(16);
-  doc.text(props.Producto.normalize('NFC'), 105, y, { align: 'center', maxWidth: 180 });
-  y += 8;
-  doc.setFontSize(12);
-  doc.text(props.CodProducto.normalize('NFC'), 105, y, { align: 'center', maxWidth: 180 });
-  y += 8;
-
-  doc.text(props.Descripcion.normalize('NFC'), 105, y, { align: 'center', maxWidth: 180 });
-  y += 8;
+  doc.setFont("times", "bold");
+  doc.setFontSize(35);
   doc.text(
-    `Medida: ${props.Medida.normalize('NFC')} | Marca: ${marca.value}`.normalize('NFC'),
+    `Presupuesto - Fecha: ${new Date().toLocaleDateString()}`.normalize("NFC"),
     105,
     y,
     {
-      align: 'center',
-      maxWidth: 180,
-    },
+      align: "center",
+    }
   );
-  y += 8;
-  doc.text(
-    `Precios: Lista ${formatPrice(precioLista.value).normalize('NFC')} | Débito ${formatPrice(
-      precioDebito.value,
-    ).normalize('NFC')} | Contado ${formatPrice(precioContado.value)}`.normalize('NFC'),
-    105,
-    y,
-    { align: 'center', maxWidth: 180 },
-  );
-  y += 8;
-  doc.text(
-    `Stock: ${props.Stock > 0 ? `${props.Stock} unidades` : 'Sin stock'}`.normalize('NFC'),
-    105,
-    y,
-    { align: 'center', maxWidth: 180 },
-  );
-  y += 8;
-  doc.text(`Fecha de consulta: ${new Date().toLocaleDateString()}`.normalize('NFC'), 105, y, {
-    align: 'center',
+
+  y += 12;
+  doc.setFontSize(22);
+  doc.text(props.Producto.normalize("NFC"), 105, y, { align: "center", maxWidth: 180 });
+  y += 12;
+  doc.setFontSize(28);
+  doc.text(props.CodProducto.normalize("NFC"), 105, y, {
+    align: "center",
     maxWidth: 180,
   });
+  y += 12;
+  doc.setFontSize(20);
+  doc.text(props.Descripcion.normalize("NFC"), 105, y, {
+    align: "center",
+    maxWidth: 180,
+  });
+  y += 20;
+  doc.text(
+    `Medida: ${props.Medida.normalize("NFC")} | Marca: ${marca.value}`.normalize("NFC"),
+    105,
+    y,
+    {
+      align: "center",
+      maxWidth: 180,
+    }
+  );
+  y += 10;
+  doc.setFontSize(16);
+  doc.text(
+    `Precios: Lista ${formatPrice(precioLista.value).normalize(
+      "NFC"
+    )} | Débito ${formatPrice(precioDebito.value).normalize(
+      "NFC"
+    )} | Contado ${formatPrice(precioContado.value)}`.normalize("NFC"),
+    105,
+    y,
+    { align: "center", maxWidth: 180 }
+  );
+  y += 15;
+  doc.text(
+    `Stock: ${props.Stock > 0 ? `${props.Stock} unidades` : "Sin stock"}`.normalize(
+      "NFC"
+    ),
+    105,
+    y,
+    { align: "center", maxWidth: 180 }
+  );
+  y += 15;
+  doc.text(
+    `Fecha de consulta: ${new Date().toLocaleDateString()}`.normalize("NFC"),
+    105,
+    y,
+    {
+      align: "center",
+      maxWidth: 180,
+    }
+  );
   y += 10;
 
-  doc.setFontSize(10);
+  doc.setFontSize(20);
   doc.setTextColor(150);
-  doc.text(`Abril vive en vos!`.normalize('NFC'), 105, y, { align: 'center', maxWidth: 180 });
+  doc.text(`Abril vive en vos!`.normalize("NFC"), 105, y, {
+    align: "center",
+    maxWidth: 180,
+  });
   y += 6;
-  doc.text('Sujeto a modificación sin previo aviso'.normalize('NFC'), 105, y, {
-    align: 'center',
+  doc.text("Sujeto a modificación sin previo aviso".normalize("NFC"), 105, y, {
+    align: "center",
     maxWidth: 180,
   });
 
-  doc.save(`${props.CodProducto || 'presupuesto'}.pdf`);
+  doc.save(`${props.CodProducto || "presupuesto"}.pdf`);
 };
 
 // Filtrar y agrupar los datos por CodForPago
@@ -180,7 +210,7 @@ const groupedTarjetas = computed(() => {
       }
 
       // Filtrar cuotas solo si CodForPago es "CRE"
-      if (tarjeta.CodForPago === 'CRE') {
+      if (tarjeta.CodForPago === "CRE") {
         const cuotasPermitidas = [1, 3, 6, 12, 15, 18];
         if (cuotasPermitidas.includes(tarjeta.NCuota)) {
           acc[tarjeta.CodForPago].push(tarjeta);
@@ -199,8 +229,10 @@ const calculateTotal = (tarjeta: any) => {
   return cuota * tarjeta.NCuota;
 };
 const getCuota = (tarjeta: any) => {
-  return (precioLista.value * (1 + (tarjeta.Interes / 100) * tarjeta.NCuota)) / tarjeta.NCuota;
+  return (
+    (precioLista.value * (1 + (tarjeta.Interes / 100) * tarjeta.NCuota)) / tarjeta.NCuota
+  );
 };
-const marca = computed(() => findMarcasById(props.CodMarca)?.Marca || 'Sin marca');
+const marca = computed(() => findMarcasById(props.CodMarca)?.Marca || "Sin marca");
 const getTotal = (tarjeta: any) => getCuota(tarjeta) * tarjeta.NCuota;
 </script>
